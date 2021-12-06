17f9d65db6df310db2d8939d49a0b6b242d149a6
bigsdawg-2k <73724274+bigsdawg-2k@users.noreply.github.com>
2021-12-05 19:16:54 -0500

    First LEDs in chain blinking
    Compile works with VS code and Platformio.  Expected first 10 LEDs to blink red but instead the first 8 are blinking red, white (RGB composite), blue, red, white (RGB composite), blue, red, green.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,44 @@
 #include <Arduino.h>
+#include <FastLED.h>
 
 // GPIO for LED on development board
-#define LED_BUILTIN 2
+#define PIN_LED_BUILTIN 2
+
+// LED related defines
+#define NUM_LEDS 10
+#define PIN_DATA 23
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
 
 void setup() {
+  
   // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PIN_LED_BUILTIN, OUTPUT);
+
+  // Setup LED strip
+  FastLED.addLeds<SK6812, PIN_DATA, GRB>(leds, NUM_LEDS);  // GRB ordering is typical
+
 }
 
 void loop() {
+  
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
+  // Builtin LED is left in as a sanity check
+  digitalWrite(PIN_LED_BUILTIN, HIGH);
+  for (int idx = 0; idx < NUM_LEDS; ++idx) {
+    leds[idx] = CRGB::Red;
+  }
+  FastLED.show();
+
   delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
+
+  digitalWrite(PIN_LED_BUILTIN, LOW);
+  for (int idx = 0; idx < NUM_LEDS; ++idx) {
+    leds[idx] = CRGB::Black;
+  }
+  FastLED.show();
+  
   delay(1000);
+ 
 }
