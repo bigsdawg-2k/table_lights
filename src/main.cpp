b22b7ef849f25f144cf3cb872d28030acab34144
bigsdawg-2k <73724274+bigsdawg-2k@users.noreply.github.com>
2021-12-06 21:45:09 -0500

    Added support for RGBW
    Implemented hack from https://www.partsnotincluded.com/fastled-rgbw-neopixels-sk6812/ that enables use of RGBW LEDs.

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,59 @@
 #include <Arduino.h>
 #include <FastLED.h>
+#include <FastLED_RGBW.h>
 
 // GPIO for LED on development board
 #define PIN_LED_BUILTIN 2
 
 // LED related defines
-#define NUM_LEDS 10
-#define PIN_DATA 23
+#define NUM_LEDS 20
+#define DATA_PIN 23
 
-// Define the array of leds
-CRGB leds[NUM_LEDS];
+// Define the array of LEDs RGBW struct
+CRGBW leds[NUM_LEDS];
+CRGB *ledsRGB = (CRGB *) &leds[0];
 
 void setup() {
   
-  // put your setup code here, to run once:
-  pinMode(PIN_LED_BUILTIN, OUTPUT);
+    // put your setup code here, to run once:
+    pinMode(PIN_LED_BUILTIN, OUTPUT);
 
-  // Setup LED strip
-  FastLED.addLeds<SK6812, PIN_DATA, GRB>(leds, NUM_LEDS);  // GRB ordering is typical
+    // Setup LED strip for FastLED with RGBW
+    // FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);  // This is the unmodified call for reference
+    FastLED.addLeds<WS2812B, DATA_PIN, RGB>(ledsRGB, getRGBWsize(NUM_LEDS));
 
 }
 
 void loop() {
   
-  // put your main code here, to run repeatedly:
-  // Builtin LED is left in as a sanity check
-  digitalWrite(PIN_LED_BUILTIN, HIGH);
-  for (int idx = 0; idx < NUM_LEDS; ++idx) {
-    leds[idx] = CRGB::Red;
-  }
-  FastLED.show();
+    // put your main code here, to run repeatedly:
+    // Builtin LED is left in as a sanity check
+    for (int dot = 0; dot < NUM_LEDS; dot++) { 
+    
+        leds[dot] = CRGB::Blue;
+        FastLED.show();
+    
+        // clear this led for the next time around the loop
+        leds[dot] = CRGB::Black;
+    
+        // Builtin LED is left in as a sanity check
+        if (dot % 2 == 0) {
+            digitalWrite(PIN_LED_BUILTIN, HIGH);
+        } else {
+            digitalWrite(PIN_LED_BUILTIN, LOW);
+        }
+    
+        delay(100);
 
-  delay(1000);
+    }
 
-  digitalWrite(PIN_LED_BUILTIN, LOW);
-  for (int idx = 0; idx < NUM_LEDS; ++idx) {
-    leds[idx] = CRGB::Black;
-  }
-  FastLED.show();
+    for (int idx = 0; idx < NUM_LEDS; ++idx) {
+        leds[idx] = CRGB::Black;
+    }
+    
+    FastLED.show();
+    digitalWrite(PIN_LED_BUILTIN, LOW);
   
-  delay(1000);
+    delay(100);
  
 }
