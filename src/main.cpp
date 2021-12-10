cf2e464ca34b64155cbdb66af5fcb8cce8a7a8aa
bigsdawg-2k <73724274+bigsdawg-2k@users.noreply.github.com>
2021-12-10 18:02:44 -0500

    Added support for BLE connection
    BLE connection and serial data transfer working.  No connection between serial and LED yet.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
+
 #include <FastLED.h>
 #include <FastLED_RGBW.h>
+
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
 
 // GPIO for LED on development board
 #define PIN_LED_BUILTIN 2
@@ -13,8 +19,79 @@
 CRGBW leds[NUM_LEDS];
 CRGB *ledsRGB = (CRGB *) &leds[0];
 
+// BLE server setup
+BLEServer *pServer = NULL;
+BLECharacteristic * pTxCharacteristic;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint8_t txValue = 0;
+
+#define SERVICE_UUID           "242FC2C3-FC6A-4D48-8C86-6DC143A5d684"
+#define CHARACTERISTIC_UUID_RX "1730e01c-ec0d-48b9-a8ee-5c6f96c652c7"
+#define CHARACTERISTIC_UUID_TX "f5b1ad65-8492-4175-a8ba-da839ee5638f"
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string rxValue = pCharacteristic->getValue();
+
+      if (rxValue.length() > 0) {
+        Serial.println("*********");
+        Serial.print("Received Value: ");
+        for (int i = 0; i < rxValue.length(); i++)
+          Serial.print(rxValue[i]);
+
+        Serial.println();
+        Serial.println("*********");
+      }
+    }
+};
+
 void setup() {
-  
+
+    Serial.begin(115200);
+
+    // Create the BLE Device
+    BLEDevice::init("UART Service");
+
+    // Create the BLE Server
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
+
+    // Create the BLE Service
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+
+    // Create a BLE Characteristic
+    pTxCharacteristic = pService->createCharacteristic(
+	    CHARACTERISTIC_UUID_TX,
+		BLECharacteristic::PROPERTY_NOTIFY
+		);
+                      
+    pTxCharacteristic->addDescriptor(new BLE2902());
+
+    BLECharacteristic * pRxCharacteristic = pService->createCharacteristic(
+	    CHARACTERISTIC_UUID_RX,
+		BLECharacteristic::PROPERTY_WRITE
+		);
+
+    pRxCharacteristic->setCallbacks(new MyCallbacks());
+
+    // Start the service
+    pService->start();
+
+    // Start advertising
+    pServer->getAdvertising()->start();
+    Serial.println("Waiting a client connection to notify...");
+
     // put your setup code here, to run once:
     pinMode(PIN_LED_BUILTIN, OUTPUT);
 
@@ -26,6 +103,28 @@ void setup() {
 
 void loop() {
   
+    if (deviceConnected) {
+        //pTxCharacteristic->setValue(&txValue, 1);
+        uint8_t a = 83;
+        pTxCharacteristic->setValue(&a, 1);
+        pTxCharacteristic->notify();
+        txValue++;
+		delay(10); // bluetooth stack will go into congestion, if too many packets are sent
+	}
+
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("start advertising");
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+		// do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
+    
     // put your main code here, to run repeatedly:
     // Builtin LED is left in as a sanity check
     for (int dot = 0; dot < NUM_LEDS; dot++) { 
