cf3cd9f76c777cce5980ae46e9f0828a6d9dda14
bigsdawg-2k <73724274+bigsdawg-2k@users.noreply.github.com>
2022-01-02 15:58:53 -0500

    WIP: Starting to build in some RTOS tasks and restructure to use command buffer

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,11 @@
 #define NUM_BYTES_COMMAND_BUFF 1024
 #define NUM_BYTES_SERIAL_BT_BUFF 1024
 
-// LED pattern
-// 0: off
-// 1: static on
-int ledPattern = 0;
+// LED FSM State
+#define FSM_STATE_LED_RESET     0
+#define FSM_STATE_LED_IDLE      1
+#define FSM_STATE_LED_STATIC    2
+int fsmState_led = FSM_STATE_LED_RESET;
 
 // Define the array of LEDs RGBW struct
 CRGBW leds[NUM_LEDS];
@@ -31,39 +32,121 @@ BluetoothSerial SerialBT;
 
 // Buffer that will be used to store commands received over BT
 CmdBuffer* cmdBuf = new CmdBuffer(NUM_BYTES_SERIAL_BT_BUFF);
+SemaphoreHandle_t xBinSem_cmdBuf;
 
 // Miscellaneous buffer for string processing
 char strBuffer [1024];
 
-void BTEventCallback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
-    switch (event) {
-    case ESP_SPP_SRV_OPEN_EVT:
-        Serial.println("Client Connected");
-        break;
-    case ESP_SPP_CLOSE_EVT:
-        Serial.println("Client disconnected");
-        break;
-    case ESP_SPP_DATA_IND_EVT:
-        Serial.println("Received data: ");
-        
-        // TODO sanitize buffer room
-        //std::copy(param->data_ind.data, param->data_ind.data + param->data_ind.len, commandBuffer + commandBufferBytes);
-        //commandBufferBytes += param->data_ind.len;
-        if(cmdBuf->getFreeSerBuf() >= param->data_ind.len){
-            cmdBuf->write((char *)param->data_ind.data, param->data_ind.len);
-        } 
-        else {
-            // Eventually do something more useful here
-            Serial.println("Buffer overflow detected in serial buffer");
-        }
-        break;
-    default:
-        Serial.println("Other event!");
+
+/**********************************
+/* Bluetooth callbacks            *  
+/**********************************/
+
+/* Bluetooth Event Callback
+ * Handles events from BT:
+ *      ESP_SPP_SRV_OPEN_EVT
+ *      ESP_SPP_CLOSE_EVT
+ *      ESP_SPP_DATA_IND_EVT
+ *          - Received data - add it to the command buffer
+ * Arguments:
+ *      event
+ *      parameter
+ * Returns: 
+ *      Nothing
+ */
+void BTEventCallback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param)
+{
+    switch (event) 
+    {
+        case ESP_SPP_SRV_OPEN_EVT:
+            Serial.println("Client Connected");
+            break;
+        case ESP_SPP_CLOSE_EVT:
+            Serial.println("Client disconnected");
+            break;
+        case ESP_SPP_DATA_IND_EVT:
+            Serial.println("Received data: ");
+            
+            // TODO sanitize buffer room
+            //std::copy(param->data_ind.data, param->data_ind.data + param->data_ind.len, commandBuffer + commandBufferBytes);
+            //commandBufferBytes += param->data_ind.len;
+            if (cmdBuf->getFreeSerBuf() >= param->data_ind.len)
+            {
+                // TODO replace hardcoded 10ms delay
+                if (xSemaphoreTake(xBinSem_cmdBuf, 10 / portTICK_PERIOD_MS) == pdTRUE)
+                {
+                    cmdBuf->write((char *)param->data_ind.data, param->data_ind.len);
+                    xSemaphoreGive(xBinSem_cmdBuf);
+                } 
+            } 
+            else
+            {
+                // TODO: Something useful here
+                Serial.println("Buffer overflow detected in serial buffer");
+            }
+            break;
+        default:
+            Serial.println("Other event!");
     }
 }
 
+
+/**********************************
+ * Application tasks              *  
+ **********************************/
+
+/* Update the LED state machine when there is a command in the command buffer 
+ *
+ * Arguments:
+ *      None
+ * Returns: 
+ *      Nothing
+ */ 
+void appTask_updateLedFsmState (void * parameter)
+{
+    // TODO: Placeholder 'code'
+    for(;;)
+    {
+        if (cmdBuf->getOccupied() > 0 && 
+            xSemaphoreTake(xBinSem_cmdBuf, 10 / portTICK_PERIOD_MS)) // TODO remove hardcoded 10ms delay
+        {
+            cmdBuf->
+            xSemaphoreGive(xBinSem_cmdBuf);
+        }
+        Serial.println("Nothing to report...");
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // TODO remove hardcoded 1s delay
+    }
+    
+    // Delete a task when finished 
+    vTaskDelete( NULL );
+   
+}
+
+/* Update LED state based on FSM state
+ *
+ * Arguments:
+ *      None
+ * Returns: 
+ *      Nothing
+ */ 
+void appTask_updateLeds (void * parameter)
+{
+    // TODO: Placeholder 'code'
+    for(;;)
+    {
+        Serial.println("this is another Task");
+        delay(1000);
+    }
+    
+    // Delete a task when finished 
+    vTaskDelete( NULL );
+   
+}
+
+
 // This gets set as the default handler, and gets called when no other command matches.
-void Unrecognized(const char *command) {
+void Unrecognized (const char *command) 
+{
     Serial.print("Unrecognized command: ");
     Serial.println(command);
 }
@@ -72,18 +155,26 @@ void UpdateLedPattern (int pattern) {
     
     switch ( pattern )
     {
-    case 0:
-    case 1:
-        ledPattern = pattern;
-        break;
-    default:
-        sprintf(strBuffer, "Unrecognized state: %d", pattern);
-        Serial.println(strBuffer);
+        case 0:
+        case 1:
+            fsmState_led = pattern;
+            break;
+        default:
+            sprintf(strBuffer, "Unrecognized state: %d", pattern);
+            Serial.println(strBuffer);
     }
     
 }
 
-void setup() {
+/* Setup function
+ *
+ * Arguments:
+ *      None
+ * Returns: 
+ *      Nothing
+ */
+void setup() 
+{
 
     // Serial over USB settings
     Serial.begin(115200);
@@ -97,6 +188,39 @@ void setup() {
         Serial.println("Bluetooth initialized");
     }
 
+    // Semaphores
+    Serial.write("Creating semaphore...");
+    if (xBinSem_cmdBuf == NULL)
+    {
+        xBinSem_cmdBuf = xSemaphoreCreateBinary();
+        if (xBinSem_cmdBuf == NULL)
+        {
+            Serial.write("Semaphore is null.");
+        }
+        else
+        {
+            Serial.write("Created semaphore!");
+        }
+        
+    }
+    
+    // RTOS Tasks
+    xTaskCreate(
+        appTask_updateLedFsmState,  // Function name
+        "Update LED FSM State",     // Task name
+        10000,                      // Stack size
+        NULL,                       // Task parameter
+        1,                          // Task priority
+        NULL);                      // Task handle
+
+    xTaskCreate(
+        appTask_updateLeds,         // Function name
+        "Update LEDs",              // Task name
+        10000,                      // Stack size
+        NULL,                       // Task parameter
+        1,                          // Task priority
+        NULL);                      // Task handle    
+
     // Physical LED on development kit
     pinMode(PIN_LED_BUILTIN, OUTPUT);
 
@@ -106,20 +230,31 @@ void setup() {
     
 }
 
-void loop() {
+/* Forever loop that is invoked by ESP32 loopTask
+ *
+ * Arguments:
+ *      None
+ * Returns: 
+ *      Nothing
+ */
+void loop() 
+{
+    
+    int i;
     
     // put your main code here, to run repeatedly:
     // Builtin LED is left in as a sanity check
-    for (int dot = 0; dot < NUM_LEDS; dot++) { 
+    for (i = 0; i < NUM_LEDS; i++) 
+    { 
     
-        leds[dot] = CRGB::Blue;
+        leds[i] = CRGB::Blue;
         FastLED.show();
     
         // clear this led for the next time around the loop
-        leds[dot] = CRGB::Black;
+        leds[i] = CRGB::Black;
     
         // Builtin LED is left in as a sanity check
-        if (dot % 2 == 0) {
+        if (i % 2 == 0) {
             digitalWrite(PIN_LED_BUILTIN, HIGH);
         } else {
             digitalWrite(PIN_LED_BUILTIN, LOW);
@@ -129,8 +264,9 @@ void loop() {
 
     }
 
-    for (int idx = 0; idx < NUM_LEDS; ++idx) {
-        leds[idx] = CRGB::Black;
+    for (i = 0; i < NUM_LEDS; ++i) 
+    {
+        leds[i] = CRGB::Black;
     }
     
     FastLED.show();
@@ -143,7 +279,8 @@ void loop() {
     //    numCmds = ;
     //}
 
-    if (cmdBuf->getOccupied() > 0) {
+    if (cmdBuf->getOccupied() > 0) 
+    {
         // TODO How to make sure there are no conflicts when reading out the buffer
         Serial.print("Buffer: ");
         //cmdBuf->readToEnd((uint8_t *)strBuffer);
@@ -152,7 +289,7 @@ void loop() {
     }
     
     // Display the current LED state
-    sprintf(strBuffer, "LED Pattern: %d", ::ledPattern);
+    sprintf(strBuffer, "LED Pattern: %d", ::fsmState_led);
     Serial.println(strBuffer);
  
 }
